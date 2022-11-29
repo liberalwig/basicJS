3b4eef36477e91cdece30555325e60bf3ad446bb
최승은 <liberalfilm@gmail.com>
2022-11-29 20:56:53 +0900

    2022-11-29-Tue-20:56

---
+++ out/LivingCoding/livingCoding14.js
@@ -0,0 +1,11 @@
+<!--   생활 코딩 중 자바스크립트 --> <!-- 13_비교(02: ==와 ===)-->
+
+
+// == : 동등연산자 [equal operator]
+alert(1 == 2); //output: false
+alert(1 == 1); //output: true
+alert("on" = 'on'); //output: true
+
+// === : [string equal operator] 데이터형식마저 일치해야 true
+alert(1 == "1"); //output: true
+alert(1 === "1"); // output: false
