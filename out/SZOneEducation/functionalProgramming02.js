b1cd9142f11afbbc86ef23868034bd1c67412180
최승은 <liberalfilm@gmail.com>
2022-12-06 12:23:30 +0900

    2022-12-06-Tue-11:45

--- out/SZOneEducation/functionalProgramming02.js
+++ out/SZOneEducation/functionalProgramming02.js
@@ -12,3 +12,44 @@ function _pipe() {
         }, arg);
     }
 }
+
+var slice = Array.prototype.slice;
+
+function _rest(list, num) {
+    return slice.call(list, num || 1);
+}
+
+
+function _reduce(list, iter, memo) {
+    if (arguments.length == 2) {
+        memo = list[0];
+        list = _rest(list);
+    }
+    _each(list, function (val) {
+        memo = iter(memo, val);
+    });
+    return memo;
+}
+
+
+function _filter(list, predi) {
+    var new_list = [];
+    _each(list, function (val) {
+        if (predi(val)) {
+            new_list.push(val);
+        }
+    });
+    return new_list;
+}
+
+function _map(list, mapper) {
+    var new_list = [];
+    _each(list, function (val, key) {
+        new_list.push(mapper(val, key)
+        )
+    });
+}
+
+
+
+
